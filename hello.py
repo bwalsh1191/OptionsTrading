954cb5283a171283a14d5cb35d576510ef649e30
Brian <bwalshgogiants@gmail.com>
2018-08-13 11:56:02 -0400

    Saved Work

---
+++ hello.py
@@ -0,0 +1,7 @@
+import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
--- master.py
+++ master.py
@@ -3,13 +3,20 @@ import stockTwits
 import alyien
 import json 
 import requests
+import Flask
 
-symbol = raw_input("Enter a stock symbol: ").upper()
 
-stockInfo.get_stock_data(symbol)
-stMessages = stockTwits.get_stockTwitsBody(symbol)
-stSentiment = stockTwits.get_stockTwitsSentiment(symbol)
+
+#symbol = raw_input("Enter a stock symbol: ").upper()
+
+#stockInfo.get_stock_data(symbol)
+#stMessages = stockTwits.get_stockTwitsBody(symbol)
+#stSentiment = stockTwits.get_stockTwitsSentiment(symbol)
 #print stMessages
-print stSentiment
+#print stSentiment
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
