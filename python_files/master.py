8de04c0588e4d2a28342d34ce357f14feb6fad6e
Brian <bwalshgogiants@gmail.com>
2018-09-13 21:45:56 -0400

    Added Timers for updating based on time of day and day of week
    Also will fire on command for the one of the arrays based on different symbols

--- python_files/master.py
+++ python_files/master.py
@@ -1,20 +1,31 @@
-import stocksapi as stockInfo
+import stocksApi as stockInfo
 import stockTwits
 import alyien
 import json 
 import requests, time
+from datetime import datetime
 
 
 
-symbol = input("Enter a stock symbol: ").upper()
+#symbol = input("Enter a stock symbol: ").upper()
 
-while True: 
-#runs this code every 60 seconds to constantly update
-    stockInfo.get_stock_data(symbol)
-    stockTwits.get_stockTwitsBody(symbol)
-    stockTwits.get_stockTwitsSentiment(symbol)
 
-    time.sleep(60)
+while True:
 
+    symbols = ["AAPL", 'AMD', "TSLA", "T"]
+    sleep_time = 0
+    num_of_week = datetime.today().weekday()
+
+    if datetime.today().replace(hour=8, minute=30, second=0, microsecond=0) <= datetime.now() <= datetime.today().replace(hour=17, minute=30, second=0, microsecond=0)and num_of_week <= 4: 
+        sleep_time = 60
+    else: 
+        sleep_time = 600
+
+    for x in range(4):
+        stockInfo.get_stock_data(symbols[x])
+        stockTwits.get_stockTwitsBody(symbols[x])
+        stockTwits.get_stockTwitsSentiment(symbols[x])
+   
+    time.sleep(sleep_time)
 
 
--- python_files/stockTwits.py
+++ python_files/stockTwits.py
@@ -8,7 +8,7 @@ import json
 import requests
 import time, datetime
 
-
+#get all the messages from StockTwits for a given stock symbol
 def get_stockTwitsBody(symbol):
     stockTwits = requests.get('https://api.stocktwits.com/api/2/streams/symbol/' + symbol + '.json')
     stockTwits_json_str = stockTwits.content
@@ -22,7 +22,7 @@ def get_stockTwitsBody(symbol):
         index+=1
     print(result)
 
-
+#get all the sentiment scores from StockTwits
 def get_stockTwitsSentiment(symbol):
     stockTwits = requests.get('https://api.stocktwits.com/api/2/streams/symbol/' + symbol + '.json')
     stockTwits_json_str = stockTwits.content
@@ -32,7 +32,7 @@ def get_stockTwitsSentiment(symbol):
     negSent = 0
     sentRatio = 0
 
-    
+    #Calculating the sentiment score from stocktwits
     for index in range (0,len(stockTwits_data['messages'])):
         sent= str(stockTwits_data['messages'][index]['entities']['sentiment'])
         if(sent == "{'basic': 'Bullish'}"):
@@ -53,16 +53,15 @@ def get_stockTwitsSentiment(symbol):
     companyCurrentPrice = stockData[symbol]['quote']['latestPrice']
 
 
-    #output is used to store all the sentiment ratio and data  
+    #gets all the date/time information for recording the information 
     ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    output = [st, symbol,posSent, negSent, totalSentiment, sentRatio,companyCurrentPrice]
-
-
-
-
+    time_stamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    num_of_week = datetime.datetime.today().weekday()
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    day_of_week = days[num_of_week]
 
     #writing this data to a file to make a running record of the data sentiment bsaed on the symbol passed
-    file_name = symbol + "_sentiment.txt"
+    output = [time_stamp, day_of_week, symbol,posSent, negSent, totalSentiment, sentRatio,companyCurrentPrice]
+    file_name = symbol.lower() + "_sentiment.txt"
     f = open("/Users/brian/Development/stock_env/OptionsTrading/stock__files/" + file_name, "a")
     f.write(str(output) + '\n')
